1d00ef4709eab3b6d58e39119adb8efaa59c132b
howloff <91728059+howloff@users.noreply.github.com>
2021-12-06 23:46:03 +0300

    Update 2.8.py

--- 2/2.8.py
+++ 2/2.8.py
@@ -3,11 +3,13 @@
 Программа должна вывести одно из чисел: 3 (если все совпадают),2 (если два совпадает) или 0 (если все числа различны)."""
 
 
-a = int(input("Введите первое число: "))
-b = int(input("Введите второе число: "))
-c = int(input("Введите третье число: "))
-if a==b and a==c :print(3)
-elif a==b :print(2)
-elif a==c :print(2)
-elif b==c:print(2)
-else:print(0)
+def func ():
+    a = int(input("Введите первое число: "))
+    b = int(input("Введите второе число: "))
+    c = int(input("Введите третье число: "))
+    if a==b and a==c : return "3"
+    elif a==b : return "2"
+    elif a==c : return "2"
+    elif b==c: return "2"
+    else: return "0"
+print (func())
