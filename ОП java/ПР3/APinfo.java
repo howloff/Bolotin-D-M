f0f0599c97f9582aa63e76815e984dce328b554e
howloff <91728059+howloff@users.noreply.github.com>
2022-06-12 22:51:49 +0300

    Add files via upload

---
+++ ОП java/ПР3/APinfo.java
@@ -0,0 +1,18 @@
+
+public class APinfo {
+	 public static void main(String[] args) {
+
+		 academicperformance AP = new academicperformance();
+		 AP.setId_studenta("666777");
+		 AP.setStudents("Danila Bolotin");
+		 AP.setFam("Bolotin");
+		 AP.setName("Danila");
+		 AP.setGroupa("U-214");
+		 AP.setDepartment("Kafedra");
+		 AP.setDiscipline("Osnovi Programmirovaniya");
+		 AP.setMark("5");
+		 AP.setNameTeacher("Grechishkin A.V.");
+
+	       System.out.println(AP.toString());
+	    }
+}
---
+++ ОП java/ПР3/academicperformance.java
@@ -0,0 +1,63 @@
+
+public class academicperformance {
+	
+
+	    private String Id_studenta;
+	    private String Students;
+	    private String Fam;
+	    private String Name;
+	    private String Groupa;
+	    private String Department;
+	    private String Discipline;
+	    private String Mark;
+	    private String NameTeacher;
+	    
+	    void setId_studenta(String Id_studenta){
+
+	        this.Id_studenta=Id_studenta;
+	    }
+
+	    void  setStudents(String Students){
+
+	        this.Students=Students;
+	    }
+
+	    void setFam (String Fam){
+
+	        this.Fam=Fam;
+	    }
+
+	    void setName (String Name){
+	        this.Name=Name;
+	    }
+
+	    void setGroupa(String Groupa){
+
+	        this.Groupa = Groupa;
+	    }
+	    
+	    void setDepartment(String Department){
+
+	        this.Department = Department;
+	    }
+	    
+	    void setDiscipline(String Discipline){
+
+	        this.Discipline = Discipline;
+	    }
+	    
+	    void setMark(String Mark){
+
+	        this.Mark = Mark;
+	    }
+	    
+	    void setNameTeacher(String NameTeacher){
+
+	        this.NameTeacher = NameTeacher;
+	    }
+	    	    
+
+	    public String toString(){
+	      return "Student performance:\n" + "Record book number - " + this.Id_studenta + "\nStudent - "+ this.Students + "\nSurname - " + this.Fam + "\nName: " + this.Name + "\nGroup - " + this.Groupa + "\nDepartment - " + this.Department + "\nDiscipline - " + this.Discipline + "\nMark - " + this.Mark + "\nNameTeacher - " + this.NameTeacher;
+	    }
+	}
